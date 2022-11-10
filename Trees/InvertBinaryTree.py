df9cc1a22a0394ee40128335a1fbeac9581d8aa5
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-10 00:10:48 -0500

    Initial Commit: Trees

---
+++ Trees/InvertBinaryTree.py
@@ -0,0 +1,11 @@
+# 226. Invert Binary Tree
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def invertTree(self, root):
+
+

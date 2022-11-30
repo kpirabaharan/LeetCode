8d54e080f079d617220f0781f1aa2e57a3adf0f0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-29 21:39:21 -0500

    Array and Hashing: 49 - Group Anagrams
    Added second solution that counts number of letters and makes that the
    map instead of a sorted version of the word. This should be faster for
    longer words (above 10 letters) I think...
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- ArrayAndHashing/GroupAnagrams.py
+++ ArrayAndHashing/GroupAnagrams.py
@@ -1,8 +1,9 @@
 from typing import List
+from time import time
 
 
-def groupAnagram(strs: List[str]) -> List[List[str]]:
-    # Solution is m*n*log(n); m = # of words, n = sort
+def groupAnagram1(strs: List[str]) -> List[List[str]]:
+    # Solution is O(m*n*log(n)); m = # of words, n = sort
     # Map to store result key is sorted word, value is list of anagrams
     result = {}
 
@@ -20,5 +21,45 @@ def groupAnagram(strs: List[str]) -> List[List[str]]:
     return result.values()
 
 
+def groupAnagram2(strs: List[str]) -> List[List[str]]:
+    # Solution is O(m*n); m = # of words, n = sort
+    # Map to store result
+    result = {}
+
+    # ASCII Value of a (97 - 122 = a - z)
+    startingASCII = 97
+
+    # Iterate through every word in list
+    for word in strs:
+
+        # Make a list where every element how many times a letter of s appears
+        # This count list will be the key of result
+        count = 26 * [0]
+
+        # Iterate through every char and add to count list
+        for c in word:
+            count[ord(c) - startingASCII] += 1
+
+        """ Convert count (list) to a tuple to make hashable (since tuples are
+        immutable and allow duplicate numbers ex. duplicate 0s/1s) """
+        if tuple(count) in result.keys():
+            result[tuple(count)].append(word)
+        else:
+            result[tuple(count)] = [word]
+
+    return result.values()
+
+
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-print(groupAnagram(strs=strs))
+
+# Run 1, Faster for shorter words
+start = time()
+Answer1 = groupAnagram1(strs=strs)
+end = time()
+print(end - start, Answer1)
+
+# Run 2
+start = time()
+Answer2 = groupAnagram2(strs=strs)
+end = time()
+print(end - start, Answer2)

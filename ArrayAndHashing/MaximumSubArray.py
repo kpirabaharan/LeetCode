b2f4693b3f0b199a95d25cf65e5b8e3601f307c7
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-12-01 01:03:45 -0500

    Array and Hashing: 54 Maximum Subarray
    O(n) solution using kadane's algorithm of dynamic programming.
    Local max of subarray[i] = max(list[i], list[i] + subarray[i-1])
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ ArrayAndHashing/MaximumSubArray.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def maxSubArray(nums: List[int]) -> int:
+    # Kadane's Algorithm of Dynamic Programming
+    # Local_max(i) = max(A(i), A(i) + Local_Max(i-1))
+    global_max = nums[0]
+    local_max = 0
+    for i in nums:
+        local_max = max(i, i + local_max)
+        if local_max > global_max:
+            global_max = local_max
+        print(local_max, global_max)
+
+    return global_max
+
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArray(nums=nums))

7312d85bbbac255c8873e5bc91dbdc102b3294d8
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-12-02 23:59:39 -0500

    Array and Hashing: 153 Find Minimum in Rotated Sorted Array
    Optimal Solution
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- ArrayAndHashing/FindMinimumInRotatedSortedArray.py
+++ ArrayAndHashing/FindMinimumInRotatedSortedArray.py
@@ -1,12 +1,29 @@
+# 153 Find Minimum in Rotated Sorted Array
 from typing import List
 
 
 def findMin(nums: List[int]) -> int:
-    for ind in range(1, len(nums)):
-        if nums[ind] < nums[ind - 1]:
-            return nums[ind]
-    return nums[0]
+    res = nums[0]
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        if nums[l] < nums[r]:
+            if nums[l] < res:
+                res = nums[l]
+            break
+
+        m = (l + r) // 2
+        if nums[m] < res:
+            res = nums[m]
+
+        if nums[m] >= nums[l]:
+            l = m + 1
+        else:
+            r = m - 1
+
+    return res
 
 
+nums = [11, 13, 15, 17]
 nums = [3, 4, 5, 1, 2]
 print(findMin(nums=nums))

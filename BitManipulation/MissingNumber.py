ee63f2611ffa7cd3d58e316551ce7c0be722cbc3
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-12-08 12:53:00 -0500

    Bit Manipulation: 190 - Reverse Bits
    Added more efficient solution.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- BitManipulation/MissingNumber.py
+++ BitManipulation/MissingNumber.py
@@ -1,4 +1,6 @@
 #  268. Missing Number
+import time
+
 
 def MissingNumber(nums: list):
     answer = 0
@@ -9,7 +11,7 @@ def MissingNumber(nums: list):
     elif len(nums) % 4 == 1:
         answer ^= 1
     elif len(nums) % 4 == 2:
-        answer ^= (len(nums) + 1)
+        answer ^= len(nums) + 1
     elif len(nums) % 4 == 3:
         answer = answer
     return answer
@@ -17,12 +19,27 @@ def MissingNumber(nums: list):
 
 def MissingNumber2(nums: list):
     length = len(nums)
-    total = length*(length+1)/2
+    total = length * (length + 1) / 2
     totalarr = sum(nums)
     return int(total - totalarr)
 
 
-nums = [0, 1, 2, 3, 4, 5, 6, 7, 9, 8, 11, 10, 13]
+def MissingNumber3(nums: list):
+    answer = 0
+    length = len(nums)
+    rem = length % 4
+    for n in nums:
+        answer ^= n
+    if rem == 0:
+        answer ^= length
+    elif rem == 1:
+        answer ^= 1
+    elif rem == 2:
+        answer ^= length + 1
+    return answer
+
+
+nums = [0, 11, 2, 3, 4, 5, 6, 1, 9, 8, 12, 10, 13]
 nums1 = [3, 0, 1]
 
 # n
@@ -30,4 +47,12 @@ nums1 = [3, 0, 1]
 # n+1
 # 0
 # repeat
+start = time.time()
+print(MissingNumber(nums))
+print("Time: ", time.time() - start)
+start = time.time()
 print(MissingNumber2(nums))
+print("Time: ", time.time() - start)
+start = time.time()
+print(MissingNumber3(nums))
+print("Time: ", time.time() - start)
--- BitManipulation/ReverseBits.c
+++ BitManipulation/ReverseBits.c
@@ -1,7 +1,7 @@
 // 190 Reverse Bits
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <time.h>
 #include <math.h>
 
 uint32_t reverseBits(uint32_t n)
@@ -16,12 +16,37 @@ uint32_t reverseBits(uint32_t n)
     return res;
 }
 
+uint32_t reverseBits2(uint32_t n)
+{
+    uint32_t res = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        res <<= 1;
+        res |= n & 1;
+        n >>= 1;
+    }
+    
+    return res;
+}
+
 int main()
 {
+    uint32_t n = 0b0000010100101000001111010011100;
+    uint32_t ans;
+    clock_t t;
+    double time_taken;
 
-    int n = 0b0000010100101000001111010011100;
+    t = clock();
+    ans = reverseBits(n);
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC;
+    printf("First - Time: %f seconds, Answer: %d\n", time_taken, ans);
 
-    printf("\n%d", reverseBits(n));
+    t = clock();
+    ans = reverseBits2(n);
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC;
+    printf("Second - Time: %f seconds, Answer: %d\n", time_taken, ans);
 
     return 0;
 }
